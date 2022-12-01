16ca6554a9e015b31d6614130404b30cea6af69b
thomaszub <thomas.zub@outlook.de>
2022-12-01 20:14:59 +0100

    Add solution for day 1

--- internal/day1/main.go
+++ internal/day1/main.go
@@ -1,7 +1,43 @@
 package day1
 
-import "fmt"
+import (
+	"embed"
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+//go:embed input.txt
+var file embed.FS
 
 func Execute() {
 	fmt.Println("*** Day 1 ***")
+	bytes, err := file.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	content := string(bytes)
+	lines := strings.Split(content, "\n")
+
+	var values []int
+	var currentValue int
+
+	for _, lineRaw := range lines {
+		line := strings.Trim(lineRaw, " \t")
+		if line == "" {
+			values = append(values, currentValue)
+			currentValue = 0
+			continue
+		}
+		value, err := strconv.Atoi(line)
+		if err != nil {
+			panic(err)
+		}
+		currentValue += value
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(values)))
+	maxValue := values[0] + values[1] + values[2]
+	fmt.Printf("The maximum values are: %d\n", maxValue)
 }
